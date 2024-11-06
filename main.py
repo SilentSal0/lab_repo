02820f9d658445bc88cb54f401bfb1da4b21182b
SilentSal0 <artur160803@gmail.com>
2024-11-06 11:25:02 +0200

    Revert "add multiply and divide functions"
    This reverts commit 8ceae9ae03952957aa60acd4a25aded260132d20.

--- main.py
+++ main.py
@@ -4,13 +4,5 @@ def sum(a, b):
 def subtract(a, b):
     return a - b
 
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    return a / b if b > 0 else "divided by zero"
-
-print('Sum:', sum(5, 5))
-print('Subraction:', subtract(5, 5))
-print('multiplication:', multiply(5, 5))
-print('Dividing:', divide(5, 0))
+print('Сума двох чисел:', sum(5, 5))
+print('Віднімання двох чисел:', subtract(5, 5))
