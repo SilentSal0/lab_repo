e7b84f79cee8008b172954834ef4f3ffd597a82b
SilentSal0 <artur160803@gmail.com>
2024-11-16 17:22:38 +0200

    add math module

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-def sum(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    return a / b if b > 0 else "divided by 0"
-
-def square(n):
-    return n * n;
+from math_module import *
 
 a = int(input('Enter a: '))
 b = int(input('Enter b: '))
